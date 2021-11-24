ab749ac980b22ae932146678b026500fdac3c0a7
johqniel <daniel.nogues.daniel@gmail.com>
2021-11-24 15:58:25 +0100

    Implementing 1D newton method.

--- optimization_algorithms.py
+++ optimization_algorithms.py
@@ -1,3 +1,38 @@
 import numpy as np
 
 # In this File we are going to implement the optimization algorithms we study in our assignment
+
+# To get some practice with implementing optimization algorithms I am going to work on an simple
+# version of the Newton procedure for function of the form f: \R \to \R
+
+# After that I want to implement a multidimensional version but that will still take some time i guess
+
+def first_derivative_(x, f, eps = 10**(-6)): 
+    """Return an approximation for the derivative of f at x. 
+
+    Args:
+        x:   a number within the domain of f 
+        A:   a function that maps a subset of \R to \R
+        eps: a scale to controll the accuracy of the approximation
+
+
+    Returns:
+        out: an approximation of the value of the derivative of f at x
+
+    """
+    out = (f(x + eps) - f(x - eps)) / eps 
+    return out
+
+def newton_method_1D(f, x_0, eps_newton = 10** (-6), eps_derivative = 10** (-6), n = 100000):
+    previous, next = x_0
+    df = lambda a : first_derivative(a,f,eps_derivative)
+    for i in range(1,n):
+        next = previous + f(previous) / df(previous)
+        previous = next 
+        if df(next) < eps_newton:
+            break
+    if i == n-1: 
+        return None
+    else: 
+        return previous
+
--- testing.py
+++ testing.py
@@ -11,18 +11,25 @@ from test_functions import rosenbrock
 from test_functions import levi_no_13
 from test_functions import griewank
 
+def plot_test_function(domain, function, name_of_function = 'some function'): 
+    """Plot a 3d graph of a function. 
 
+    Args:
+        domain:                     domain \subset \R of the function 
+        function:                   a function that returns a value for a input array [a,b] where a,b are in the domain
+        name_of_function (string) : the name of the function
+        
 
-if __name__ == "__main__":
+    Returns:
+        a 3d plot of the function
 
-    x_interval = (-5.12,5.12)
-    y_interval = (-5.12,5.12)
-    x_points = np.linspace(x_interval[0], x_interval[1],100)
-    y_points = np.linspace(y_interval[0], y_interval[1],100)
+    """
+    x_points = np.linspace(domain[0], domain[1],100)
+    y_points = np.linspace(domain[0], domain[1],100)
 
     X, Y = np.meshgrid(x_points, y_points)
 
-    rastrigin_vectorized = np.vectorize(lambda a,b : rastrigin([a,b]))
+    rastrigin_vectorized = np.vectorize(lambda a,b : function([a,b]))
     
     Z = rastrigin_vectorized(X,Y)
 
@@ -30,5 +37,13 @@ if __name__ == "__main__":
     ax = plt.axes(projection = '3d')
 
     ax.plot_surface(X, Y, Z)
-    ax.set(xlabel='x', ylabel='y', zlabel='f(x, y)', title='f = rastragin function')
+    ax.set(xlabel='x', ylabel='y', zlabel='f(x, y)', title='f = ' + name_of_function)
     plt.show()
+    pass
+
+if __name__ == "__main__":
+
+    plot_test_function((-5.12,5.12), rastrigin, 'rastrigin')
+    plot_test_function((-100,100), griewank, 'griewank')
+    plot_test_function((-10,10), levi_no_13, 'Levi no. 13')
+    plot_test_function((-100,100), rosenbrock, 'rosenbrock')
