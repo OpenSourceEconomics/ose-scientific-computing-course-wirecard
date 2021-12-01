e49128184cf79ab1c1751fac245eb2708ef51040
johqniel <daniel.nogues.daniel@gmail.com>
2021-12-01 10:21:55 +0100

    Moving code into auxiliaries folder.

--- auxiliary/optimization_algorithms.py
+++ auxiliary/optimization_algorithms.py
@@ -8,11 +8,40 @@ from autograd import grad, jacobian
 # and a naive optimization algorithms that uses the multidimensional newton method
 
 def find_starting_point(f,domain,n,k = 10000):
+    """Returns a candidate to start the local optimum finding process from.
+    Args:
+        f:              a function from \R^n to \R whose optimum we want to find
+        domain:         the domain of the function in which we want to find the point (domain ist always a cube)
+        n:              the dimension of the domain of the function
+        k:              the amount of random points we draw to run the optimization on.
+
+       
+
+
+    Returns:
+        out:            a candidate in domain^n to start the local search for an optimum from
+
+    """
     #print("find_starting point bekommt n == ", n)
     A = np.random.rand(k,n)
     B = [f(domain[0] + (domain[1]-domain[0])*x ) for x in A]
     index = np.where(B == np.amin(B) )
     return A[index][0]
+
+def initial_simplex(dim, domain):
+    """Return a dim- dimensional simplex within the cube domain^n
+    Args:
+        dim:           the dimension we are working with
+
+    Returns:
+        out:           the verticies of the simplex in an dim+1 dimensional array
+
+    """
+    A = np.random.rand(dim + 1,dim)
+    A = [domain[0] + x * (domain[1]- domain[0]) for x in A]
+    
+    return(A)
+
 
 
 
@@ -92,16 +121,35 @@ def newton_method(f, df, x_n, eps = 10**(-6), n = 1000):
     else: 
         return "Didnt converge."
 
-def nelder_mead_method(f, dim, alpha = 1, gamma = 2, rho = 0.5, sigma = 0.5):
+def nelder_mead_method(f, verts ,dim, alpha = 1, gamma = 2, rho = 0.5, sigma = 0.5):
     # Pseudo code can be found on: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method
 
     # 1 Order
 
+    values = np.array([f(vert) for vert,index in zip(verts,range(dim+1))])
+    indexes = np.argsort(values)
+
     # 2 Calculate x_0
+
+    x_0 = np.sum(np.array([verts[i] for i in indexes[:-1]])) / dim
 
     # 3 Reflection
 
+    x_r = x_0 + alpha*(x_0 - verts[indexes[-1]])
+    if values[indexes[0]] <= f(x_r) and f(x_r) < values[indexes[-2]]:
+        verts[indexes[-1]] = x_r
+        nelder_mead_method(f, verts, dim , alpha, gamma, rho, sigma)
+
     # 4 Expansion
+
+    if f(x_r) < values[indexes[0]]:
+        x_e = x_0 + gamma*(x_r - x_0)
+        if f(x_e) < f(x_r):
+            verts[verts[indexes[-1]]] = x_e
+            nelder_mead_method(f,verts,dim,alpha,gamma,rho,sigma)
+        else: 
+            verts[indexes[-1]] = x_r
+            nelder_mead_method(f, verts, dim , alpha, gamma, rho, sigma)
 
     # 5 Contraction
 
@@ -151,5 +199,10 @@ def naive_optimization(f, dim, domain, eps_newton = 10**(-6), eps_derivative = 1
 if __name__ == "__main__":
     
     test_finding_starting_point = False
+    test_initial_simplex = False
     if test_finding_starting_point == True: 
         print(find_starting_point(lambda a : a[0] + a[1], [4,6],2))
+    if test_initial_simplex == True:
+        simplex = initial_simplex(3,[5,6])
+        for vert in simplex: 
+            print(vert)
