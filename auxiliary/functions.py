11b9bc0203ccb2e262ca17e436175aa42a49d2e2
johqniel <daniel.nogues.daniel@gmail.com>
2021-12-20 15:56:42 +0100

    Renaming Files for clarity and working further on the Debbuging Infrastructure.

---
+++ auxiliary/functions.py
@@ -0,0 +1,219 @@
+import autograd.numpy as np
+import numbers
+
+# In this File we are implementing real valued functions to test the optimization algorithms we implement
+
+# The first four are taken from our main source
+
+
+def is_x_in_domain(x, domain):
+    if len(x) != len(domain):
+        assert "Dimension of input value and domain are not the same"
+    for x_i, i in zip(x, range(len(x))):
+        if domain[i][0] < x_i and x_i < domain[i][1]:
+            pass
+        else:
+            return False
+
+    return True
+
+
+class griewank_instance:
+    def __init__(self, dimension, a=200, domain=[-100, 100]):
+        self.a = 200
+        self.dim = dimension
+        self.domain = numpy.array([domain for i in range(self.dim)])
+
+    def value(x):
+        assert (
+            len(x) == self.dim
+        ), "The dimension of the input is and the function are different"
+        assert is_x_in_domain(
+            x, self.domain
+        ), "The input value is not in the domain of the function"
+        return griewank(x, self.a, self.domain)
+
+
+def griewank(x, a=200, domain=[-100, 100]):
+    """Compute the output of the x.size dimensional griewank function.
+
+    Args:
+        x: a number, an array of numbers or a numpy array of numbers
+        a: a variable factor of the function
+
+
+    Returns:
+        out: value of the x.size dimensional griewank function evaluated at x
+
+    """
+
+    # This function has been tested and should work properly
+
+    # The global minimum of this function lies at (0,...,0) and equals to 1
+
+    input = np.array(x)
+    sum = (1 / 200) * np.dot(input, input)
+    prod = np.prod(np.cos(input / np.sqrt(np.arange(1, input.size + 1, 1))))
+    out = sum - prod + 2
+    return out
+
+
+class rastrigin_instance:
+    def _init__(self, dimension, A=10, domain=[-5.12, 5.12]):
+        self.A = A
+        self.dim = dimension
+        self.domain = np.array([domain for i in range(self.dim)])
+
+    def value(x):
+        assert (
+            len(x) == self.dim
+        ), "The dimension of the input is and the function are different"
+        assert is_x_in_domain(
+            x, self.domain
+        ), "The input value is not in the domain of the function"
+        return rastrigin(x, self.A)
+
+
+def rastrigin(x, A=10, domain=[-5.12, 5.12]):
+    """Compute the output of the x.size dimensional rastrigin function.
+
+    Args:
+        x: a number, an array of numbers or a numpy array of numbers all entries of x should lie within the default domain
+        A: A factor that varies the shape of the function
+        domain: the domain of the function
+
+    Returns:
+        out: value of the x.size dimensional rastrigi function evaluated at x
+
+    """
+    # The global minimum lies at (0,...,0) and equals to 1
+
+    # This function still hast to be tested wether it does what its supposed to do
+
+    input = np.array(x)
+    n = input.size
+    out = A * n + np.sum(input * input - A * np.cos(2 * np.pi * input)) + 1
+    return out
+
+
+class rosenbrock_instance:
+    def __init__(self, dimension, domain=[-100, 100]):
+        self.dim = dimension
+        self.domain = np.array([domain for i in range(self.dim)])
+
+    def value(x):
+        assert (
+            len(x) == self.dim
+        ), "The dimension of the input is and the function are different"
+        assert is_x_in_domain(
+            x, self.domain
+        ), "The input value is not in the domain of the function"
+        return rosenbrock(x)
+
+
+def rosenbrock(x, domain=[-100, 100]):
+    """Compute the output of the x.size dimensional rosenbrock function.
+
+    Args:
+        x: a number, an array of numbers or a numpy array of numbers all entries of x should lie within the default domain
+        domain: the domain of the function
+
+    Returns:
+        out: value of the x.size dimensional rosenbrock function evaluated at x
+
+    """
+    # This function still hast to be tested wether it does what its supposed to do
+
+    # The global mimimum of this function lies at (1,..,1) and equals to 1
+
+    input = np.array(x)
+
+    if input.size <= 1:
+        return 0
+    else:
+        return (
+            np.sum(100 * (input[1:] - input[:-1] ** 2) ** 2 + (1 - input[:-1]) ** 2) + 1
+        )
+
+
+class levi_no_13_instance:
+    def __init__(self, dimension, domain=[-10, 10]):
+        self.dim = dimension
+        self.domain = np.array([domain for i in range(self.dim)])
+
+    def value(x):
+        assert (
+            len(x) == self.dim
+        ), "The dimension of the input is and the function are different"
+        assert is_x_in_domain(
+            x, self.domain
+        ), "The input value is not in the domain of the function"
+        return levi_no_13(x)
+
+
+# for now i have excluded the domain of the following function
+def levi_no_13(x):
+    """Compute the output of the x.size dimensional Levi No. 13 function.
+
+    Args:
+        x: a number, an array of numbers or a numpy array of numbers all entries of x should lie within the default domain
+        domain: the domain of the function
+
+    Returns:
+        out: value of the x.size dimensional Levi No. 13 function evaluated at x
+
+    """
+
+    # This function still hast to be tested wether it does what its supposed to do
+
+    # The global minimum of this function lies at (1,...1) and equals to 1
+
+    input = np.array(x)
+
+    if input.size > 1:
+        out_1 = np.sin(np.sin(3 * np.pi * input[0]))
+        out_2 = ((input[-1] - 1) ** 2) * (1 + np.sin(np.sin(2 * np.pi * input[-1])))
+        out_3 = np.dot(
+            ((input[:-1] - 1) ** 2), (1 + np.sin(np.sin(3 * np.pi * input[1:])))
+        )
+        out = out_1 + out_2 + out_3 + 1
+    else:
+        out = (
+            np.sin(np.sin(3 * np.pi * input))
+            + ((input - 1) ** 2) * (1 + np.sin(np.sin(2 * np.pi * input)))
+            + 1
+        )
+
+    return out
+
+
+if __name__ == "__main__":
+
+    x_1 = np.array([1, 4, np.sqrt(3)])
+    x_2 = 5
+    x_3 = np.array([1, 2, 3, 4, 5])
+
+    # print( np.prod( np.cos( x_1 / np.sqrt( np.arange(1,x_1.size +1,1) ) ) ) )
+
+    # print( griewank(x_2) )
+    print(levi_no_13(x_1))
+    print(levi_no_13(x_2))
+
+    List = [griewank, levi_no_13, rosenbrock, rastrigin]
+    inputs = np.array(
+        [
+            [1, 2, 3],
+            [5, 6, 7],
+            [-8, 5, 7],
+            [5, 2, -1],
+            [0, 1, 0],
+            [2, -4, -5],
+            [19, 17, 23],
+            [1, -1, 0],
+        ]
+    )
+    A = []
+    for function in List:
+        A.append([function(x) for x in inputs])
+    for a in A:
+        print(a)
--- auxiliary/test_functions.py
+++ auxiliary/test_functions.py
@@ -1,152 +1,58 @@
-import autograd.numpy as np
-import numbers
+import numpy as np
+import pandas as pd
 
-# In this File we are implementing real valued functions to test the optimization algorithms we implement
-
-# The first four are taken from our main source
-
-
-def is_x_in_domain(x, domain):
-    if len(x) != len(domain):
-        assert "Dimension of input value and domain are not the same"
-    for x_i, i in zip(x, range(len(x))):
-        if domain[i][0] < x_i and x_i < domain[i][1]:
-            pass
-        else:
-            return False
-
-    return True
+from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_almost_equal
 
 
-class griewank_instance:
-    def __init__(self, dimension, a=200, domain=[-100, 100]):
-        self.a = 200
-        self.dim = dimension
-        self.domain = numpy.array([domain for i in range(self.dim)])
-
-    def value(x):
-        assert (
-            len(x) != self.dim
-        ), "The dimension of the input is and the function are different"
-        assert is_x_in_domain, "The input value is not in the domain of the function"
-        return griewank(x, self.a, self.domain)
+from functions import (
+    rastrigin_instance,
+    griewank_instance,
+    levi_no_13_instance,
+    rosenbrock_instance,
+)
 
 
-def griewank(x, a=200, domain=[-100, 100]):
-    """Compute the output of the x.size dimensional griewank function.
-
-    Args:
-        x: a number, an array of numbers or a numpy array of numbers
-        a: a variable factor of the function
-
-
-    Returns:
-        out: value of the x.size dimensional griewank function evaluated at x
-
-    """
-
-    # This function has been tested and should work properly
-
-    # The global minimum of this function lies at (0,...,0) and equals to 1
-
-    input = np.array(x)
-    sum = (1 / 200) * np.dot(input, input)
-    prod = np.prod(np.cos(input / np.sqrt(np.arange(1, input.size + 1, 1))))
-    out = sum - prod + 2
-    return out
+def test_rastrigin():
+    inputs = create_inputs()
+    function = rastrigin_instance
+    expected_outs = np.array([15, 111, 139, 31, 2, 46, 1180, 3])
+    computed_outs = np.array([function.value(x) for x in inputs])
+    assert_array_almost_equal(expected_outs, computed_outs)
 
 
-def rastrigin(x, A=10, domain=[-5.12, 5.12]):
-    """Compute the output of the x.size dimensional rastrigin function.
+def test_griewank():
+    expected_outs = np.array([2.083, 2.470, 2.773, 2.112, 1.244, 2.608, 7.255, 1.6])
 
-    Args:
-        x: a number, an array of numbers or a numpy array of numbers all entries of x should lie within the default domain
-        A: A factor that varies the shape of the function
-        domain: the domain of the function
-
-    Returns:
-        out: value of the x.size dimensional rastrigi function evaluated at x
-
-    """
-    # The global minimum lies at (0,...,0) and equals to 1
-
-    # This function still hast to be tested wether it does what its supposed to do
-
-    input = np.array(x)
-    n = input.size
-    out = A * n + np.sum(input * input - A * np.cos(2 * np.pi * input)) + 1
-    return out
+    pass
 
 
-def rosenbrock(x, domain=[-100, 100]):
-    """Compute the output of the x.size dimensional rosenbrock function.
+def test_levi_no_13():
+    expected_outs = np.array([6, 78, 134, 22, 3, 63, 1065, 6])
 
-    Args:
-        x: a number, an array of numbers or a numpy array of numbers all entries of x should lie within the default domain
-        domain: the domain of the function
-
-    Returns:
-        out: value of the x.size dimensional rosenbrock function evaluated at x
-
-    """
-    # This function still hast to be tested wether it does what its supposed to do
-
-    # The global mimimum of this function lies at (1,..,1) and equals to 1
-
-    input = np.array(x)
-
-    if input.size <= 1:
-        return 0
-    else:
-        return (
-            np.sum(100 * (input[1:] - input[:-1] ** 2) ** 2 + (1 - input[:-1]) ** 2) + 1
-        )
+    pass
 
 
-# for now i have excluded the domain of the following function
-def levi_no_13(x):
-    """Compute the output of the x.size dimensional Levi No. 13 function.
+def test_rosenbrock():
+    expected_outs = np.array([202, 120242, 380598, 55418, 202, 50527, 18909781, 505])
+    pass
 
-    Args:
-        x: a number, an array of numbers or a numpy array of numbers all entries of x should lie within the default domain
-        domain: the domain of the function
 
-    Returns:
-        out: value of the x.size dimensional Levi No. 13 function evaluated at x
-
-    """
-
-    # This function still hast to be tested wether it does what its supposed to do
-
-    # The global minimum of this function lies at (1,...1) and equals to 1
-
-    input = np.array(x)
-
-    if input.size > 1:
-        out_1 = np.sin(np.sin(3 * np.pi * input[0]))
-        out_2 = ((input[-1] - 1) ** 2) * (1 + np.sin(np.sin(2 * np.pi * input[-1])))
-        out_3 = np.dot(
-            ((input[:-1] - 1) ** 2), (1 + np.sin(np.sin(3 * np.pi * input[1:])))
-        )
-        out = out_1 + out_2 + out_3 + 1
-    else:
-        out = (
-            np.sin(np.sin(3 * np.pi * input))
-            + ((input - 1) ** 2) * (1 + np.sin(np.sin(2 * np.pi * input)))
-            + 1
-        )
-
+def create_inputs():
+    out = np.array(
+        [
+            [1, 2, 3],
+            [5, 6, 7],
+            [-8, 5, 7],
+            [5, 2, -1],
+            [0, 1, 0],
+            [2, -4, -5],
+            [19, 17, 23],
+            [1, -1, 0],
+        ]
+    )
     return out
 
 
 if __name__ == "__main__":
-
-    x_1 = np.array([1, 4, np.sqrt(3)])
-    x_2 = 5
-    x_3 = np.array([1, 2, 3, 4, 5])
-
-    # print( np.prod( np.cos( x_1 / np.sqrt( np.arange(1,x_1.size +1,1) ) ) ) )
-
-    # print( griewank(x_2) )
-    print(levi_no_13(x_1))
-    print(levi_no_13(x_2))
+    test_rastrigin()
