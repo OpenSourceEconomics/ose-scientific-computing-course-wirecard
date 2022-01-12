6a4c91cf8bab78babf380dd4c06bb84705d4ea5f
johqniel <daniel.nogues.daniel@gmail.com>
2022-01-12 11:45:56 +0100

    Adding a computational budget stopping criterion for the nelder mead method

--- auxiliary/nelder_mead_based_optimization_source.py
+++ auxiliary/nelder_mead_based_optimization_source.py
@@ -17,6 +17,57 @@ def initial_simplex(dim, domain):
     return A
 
 
+def pick_terminate_criterion(x_tolerance, y_tolerance):
+    if -np.inf < x_tolerance:
+        if -np.inf < y_tolerance:
+            stopping_criterion = lambda inputs, values: terminate_criterion_x_or_y(
+                inputs, values, x_tolerance, y_tolerance
+            )
+            return stopping_criterion
+        else:
+            stopping_criterion = lambda inputs, values: terminate_criterion_x(
+                inputs, values, x_tolerance, y_tolerance
+            )
+            return stopping_criterion
+    elif -np.inf < y_tolerance:
+        stopping_criterion = lambda inputs, values: terminate_criterion_y(
+            inputs, values, x_tolerance, y_tolerance
+        )
+        return stopping_criterion
+    else:
+        assert (
+            x_tolerance != y_tolerance
+        ), "You should not set both x_tolerance and y_tolerance to -np.inf."
+
+
+def terminate_criterion_x_or_y(verts, f_difference, x_tolerance, y_tolerance):
+    if terminate_criterion_x(verts, f_difference, x_tolerance, y_tolerance):
+        if terminate_criterion_y(verts, f_difference, x_tolerance, y_tolerance):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def terminate_criterion_x(verts, f_difference, x_tolerance, y_tolerance):
+    eps = 0
+    for vert in verts:
+        eps += abs(np.linalg.norm(vert - verts[0]))
+    eps = eps / len(verts)
+    # print("Summed distance = ", eps)
+    if eps < x_tolerance:
+        return True
+    if eps > 50:
+        return True
+    else:
+        return False
+
+
+def terminate_criterion_y(verts, f_difference, x_tolerance, y_tolerance):
+    pass
+
+
 # TODO - write a nelder-mead routine for michael that takes f, startpoint x_0
 #      - computational budget/ maximale anzahl an iterationen als argument
 #      - stop criterium für terminate als stopp kriterium
@@ -107,6 +158,118 @@ def nm_terminate(verts):
         return True
     else:
         return False
+
+
+def call_nelder_mead_method(
+    f,
+    verts,
+    dim,
+    f_difference,
+    terminate_criterion=terminate_criterion_x,
+    x_tolerance=1e-6,
+    y_tolerance=1e-6,
+    alpha=1,
+    gamma=2,
+    rho=0.5,
+    sigma=0.5,
+):
+    # Pseudo code can be found on: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method
+
+    # 0 Order
+    values = np.array([f(vert) for vert in verts])
+    indexes = np.argsort(values)
+
+    x_0 = np.array([0, 0])
+    for index in indexes[:-1]:
+        x_0 = x_0 + verts[index]
+    x_0 = x_0 / (len(verts) - 1)
+
+    x_r = x_0 + alpha * (x_0 - verts[indexes[-1]])
+    x_e = x_0 + gamma * (x_r - x_0)
+    x_c = x_0 + rho * (verts[indexes[-1]] - x_0)
+
+    # 1 Termination
+
+    if terminate_criterion(verts, f_difference, x_tolerance, y_tolerance):
+        return np.round(verts[indexes[0]])
+
+    # 3 Reflection
+    if values[indexes[0]] <= f(x_r):
+        if f(x_r) < values[indexes[-2]]:
+            f_difference = abs(f(x_r) - values[indexes[0]])
+            return nelder_mead_method(
+                f, nm_replace_final(verts, indexes, x_r), dim, alpha, gamma, rho, sigma
+            )
+
+    # 4 Expansion
+
+    if f(x_r) < values[indexes[0]]:
+        # x_e = x_0 + gamma * (x_r - x_0)
+        if f(x_e) < f(x_r):
+            f_difference = abs(f(x_e) - values[indexes[0]])
+            return nelder_mead_method(
+                f,
+                nm_replace_final(verts, indexes, x_e),
+                dim,
+                f_difference,
+                terminate_criterion,
+                x_tolerance,
+                y_tolerance,
+                alpha,
+                gamma,
+                rho,
+                sigma,
+            )
+        else:
+            f_difference = abs(f(x_r) - values[indexes[0]])
+            return nelder_mead_method(
+                f,
+                nm_replace_final(verts, indexes, x_r),
+                dim,
+                f_difference,
+                terminate_criterion,
+                x_tolerance,
+                y_tolerance,
+                alpha,
+                gamma,
+                rho,
+                sigma,
+            )
+
+    # 5 Contraction
+
+    # x_c = x_0 + rho * (verts[indexes[-1]] - x_0)
+    if f(x_c) < f(verts[indexes[-1]]):
+        f_difference = abs(f(x_c) - values[indexes[0]])
+        return nelder_mead_method(
+            f,
+            nm_replace_final(verts, indexes, x_c),
+            dim,
+            f_difference,
+            terminate_criterion,
+            x_tolerance,
+            y_tolerance,
+            alpha,
+            gamma,
+            rho,
+            sigma,
+        )
+
+    # 6 Shrink
+
+    return nelder_mead_method(
+        f,
+        nm_shrink(verts, indexes, sigma),
+        dim,
+        f_difference,
+        terminate_criterion,
+        x_tolerance,
+        y_tolerance,
+        alpha,
+        gamma,
+        rho,
+        sigma,
+    )
 
 
 def nm_replace_final(verts, indexes, x_new):  # passed pytest
--- auxiliary/test_optimization.py
+++ auxiliary/test_optimization.py
@@ -14,6 +14,7 @@ from nelder_mead_based_optimization_source import (
     initial_simplex,
     nm_shrink,
     nelder_mead_method,
+    call_nelder_mead_method,
 )
 from newton_based_optimization_source import (
     newton_based_naive_optimization,
@@ -202,10 +203,47 @@ def test_nelder_mead_method():
         # print(index)
 
 
+def test_call_nelder_mead_method():
+    input_functions = [
+        lambda a: 20 * (a[0] + a[1]) ** 2 + a[1] ** 4 + 1,
+        lambda a: (1 / 200) * (a[0] + 1) ** 2 * (math.cos(a[1]) + 1) + a[1] ** 2,
+        lambda a: (1 / 800) * (a[0] - 6) ** 4 * (math.sin(a[1]) + 3) + a[1] ** 4,
+    ]
+
+    input_functions_hard = [
+        lambda a: griewank(a),
+        lambda a: rosenbrock(a),
+        lambda a: rastrigin(a),
+    ]
+
+    expected_minima = [[0, 0], [-1, 0], [6, 0]]
+
+    expected_minima_hard = [[0, 0], [1, 1], [0, 0]]
+
+    names_of_functions = [
+        "lambda a: 20 * (a[0] + a[1]) ** 2 + a[1] ** 4 + 1",
+        "lambda a: (1 / 200) * (a[0] + 1) ** 2 * (np.cos(a[1]) + 1) + a[1] ** 2",
+        "lambda a : (1/800) * ( a[0] - 6) ** 4 * (np.sin(a[1])+ 3) + a[1] ** 4",
+    ]
+    names_of_functions_hard = ["griewank", "rosenbrock", "rastrigin"]
+
+    computed_minima = [
+        call_nelder_mead_method(input_function, initial_simplex(2, [-5, 5]), 2)
+        for input_function in input_functions
+    ]
+    for expected_minimum, computed_minimum, name in zip(
+        expected_minima, computed_minima, names_of_functions
+    ):
+        # print("expected minimum: ", expected_minimum, "computed minimum: ", computed_minimum)
+
+        assert_array_almost_equal(computed_minimum, expected_minimum, err_msg=name)
+        # print(index)
+
+
 if __name__ == "__main__":
     # test_functions_used_here()
     test_nelder_mead_method()
-
+    test_call_nelder_mead_method()
     test_old_newton_opti()
     # test_newton_based_optimization()
     print("All tests completed")
